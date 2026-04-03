ae3e54b80ab00ff8eefbb25580d89a63c9fdf96b
chenmeng-lk <2967845704@qq.com>
2026-04-03 17:18:09 +0800

    more instances

---
+++ instance/HFVRP/X148-HVRP.sol
@@ -0,0 +1,56 @@
+Route #1: 55 22
+Route #2: 99
+Route #3: 5
+Route #4: 47
+Route #5: 60 4
+Route #6: 83
+Route #7: 69 63 24
+Route #8: 98 7 57
+Route #9: 118
+Route #10: 124 53
+Route #11: 71 145
+Route #12: 64 94
+Route #13: 114 125
+Route #14: 138 102
+Route #15: 39 43
+Route #16: 136 104
+Route #17: 54 10 68
+Route #18: 45 51
+Route #19: 23 46
+Route #20: 82 32
+Route #21: 25 29
+Route #22: 36 66 13 33
+Route #23: 110 137
+Route #24: 6 48 119
+Route #25: 30 19
+Route #26: 49 34
+Route #27: 44
+Route #28: 3 12
+Route #29: 111 27 86
+Route #30: 67 61 14
+Route #31: 100 115 135
+Route #32: 59 1
+Route #33: 50 9 11 38
+Route #34: 122 84
+Route #35: 106 129
+Route #36: 76 28 97
+Route #37: 26 130
+Route #38: 144 65
+Route #39: 8 37 105 40 52
+Route #40: 35 70 113 142
+Route #41: 87 95 121
+Route #42: 131 16 117 74 15
+Route #43: 92 79 139 91 96
+Route #44: 126 73 89
+Route #45: 109 17 112 2
+Route #46: 31 108 90 116
+Route #47: 128 146 123
+Route #48: 42 75 18 88
+Route #49: 58 72 93 21
+Route #50: 107 134 77 127 85 62
+Route #51: 20 81 120 80 103 101
+Route #52: 56 132 143 140
+Route #53: 41 78 133 141 147
+Route #54:
+Route #55:
+Cost: 8028527330
